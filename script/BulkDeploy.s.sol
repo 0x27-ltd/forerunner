f360a6ca8d71cf301b3ea40c220f98cb4b2e844b
jonovdm <vdm.jono@gmail.com>
2023-07-04 11:27:56 +0300

    add bulk deploy script

--- script/BulkDeploy.s.sol
+++ script/BulkDeploy.s.sol
@@ -9,6 +9,7 @@ import "../src/ERC20Decimal.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "../src/MockSafe.sol";
 import "../src/IGnosisSafe.sol";
+import "../src/IGnosisSafeL2.sol";
 import "../src/IGnosisSafeProxyFactory.sol";
 import "forge-std/Test.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
@@ -19,23 +20,34 @@ import "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 //This also had some safe stuff that may be useful down the line:
 //https://github.com/emo-eth/create2-helpers
 
+//Safe deployments
+//https://github.com/safe-global/safe-deployments/blob/bf35d97bfbc63ad98b4f70fcb9d44aebfe6d3804/src/assets/v1.3.0/gnosis_safe_l2.json
+
 //forge script script/BulkDeploy.s.sol:BulkDeployScript --rpc-url $LOCAL_HOST --broadcast -vvvv
 contract BulkDeployScript is Script, Test {
-    //@note this is for MATIC!!!
+    //@note this script is chain specific!!!
     function setUp() public {}
 
     address[] public owners; //keep this! read why in initData
 
     function run() public {
-        address manager = vm.addr(vm.envUint("FOX_PRIVATE_KEY"));
-        address investor = vm.addr(vm.envUint("INVESTOR_PRIVATE_KEY"));
-        vm.startBroadcast(vm.envUint("FOX_PRIVATE_KEY")); //don't need to pass a private-key in here as we pass --private-key $PRIVATE_KEY in cmd line
+        // address maticMasterSafeL1 = 0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552;
+        // address maticSafeProxyFactory = 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2;
+        // address maticUSDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+        // address DEFAULT_FALLBACK_HANDLER_ADDRESS = 0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4;
+        address arbMasterSafeL2 = 0x3E5c63644E683549055b9Be8653de26E0B4CD36E;
+        address arbFallbackHandler = 0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4;
+        address arbSafeProxyFactory = 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2;
+        address arbUSDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+
+        address manager = vm.addr(vm.envUint("FUNDMODULE_MANAGER"));
+        // address investor = vm.addr(vm.envUint("INVESTOR_PRIVATE_KEY"));
+        vm.startBroadcast(vm.envUint("FUNDMODULE_MANAGER")); //another option is --private-key $PRIVATE_KEY in cmd line when the script only needs one pk
 
         //1.) Deploy Safe (or can use ui)
-        //mastercopy @audit I am creating an L1 style gnosis safe here - might want to change this later
-        //Gnosis safe UI for polygon uses the L2 one from what I can tell so the ui doesn't work perfectly with L1 style apparently
-        IGnosisSafe masterSafe = IGnosisSafe(0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552);
-        address DEFAULT_FALLBACK_HANDLER_ADDRESS = 0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4;
+        //@note chose your mastercopy - L2 for all cheap chains!
+        // IGnosisSafe masterSafe = IGnosisSafe(maticMasterSafeL1);
+        IGnosisSafeL2 masterSafe = IGnosisSafeL2(arbMasterSafeL2);
 
         owners.push(manager);
         bytes memory initData = abi.encodeWithSelector(
@@ -44,27 +56,27 @@ contract BulkDeployScript is Script, Test {
             1, //threshold
             address(0),
             new bytes(0),
-            DEFAULT_FALLBACK_HANDLER_ADDRESS,
+            arbFallbackHandler,
             address(0),
             0,
             payable(address(0))
         );
-        IGnosisSafeProxyFactory proxyFactory = IGnosisSafeProxyFactory(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2);
+        IGnosisSafeProxyFactory proxyFactory = IGnosisSafeProxyFactory(arbSafeProxyFactory);
         address safeProxy = proxyFactory.createProxyWithNonce(address(masterSafe), initData, block.timestamp);
         // emit log_bytes(initData);
         //get correct calldata: cast calldata "setup(address[],uint256,address,bytes,address,address,uint256,address)" "[0xED2955881557A1951A9D1BDb346443657103a2a4]" 1 0x0000000000000000000000000000000000000000 0x 0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4 0x0000000000000000000000000000000000000000 0 0x0000000000000000000000000000000000000000
         //cast --calldata-decode "setup(address[],uint256,address,bytes,address,address,uint256,address)"
 
         //2.) Deploy Module
-        address accountant = vm.addr(vm.envUint("FOX_PRIVATE_KEY"));
-        IERC20 usdc = IERC20(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
+        address accountant = vm.addr(vm.envUint("FUNDMODULE_ACCOUNTANT"));
+        IERC20 usdc = IERC20(arbUSDC);
 
         FundModule fundModule = new FundModule(
-            "VAULT Token",
+            "VAULT",
             "VLT",
-            manager,
-            //intentionally setting accountant below as manager just for easier testing
-            manager,
+            manager, //@audit
+            //can set accountant below as manager too for easier testing
+            accountant,
             safeProxy,
             address(usdc)
         );
@@ -92,18 +104,18 @@ contract BulkDeployScript is Script, Test {
         bytes memory enableData = abi.encodeWithSelector(IGnosisSafe.enableModule.selector, address(fundModule));
         safe.execTransaction(address(safeProxy), 0, enableData, 0, 0, 0, 0, address(0), address(0), paddedData);
 
-        //4.) WL investor
-        fundModule.addToWhitelist(investor);
-        vm.stopBroadcast();
+        // //4.) WL investor
+        // fundModule.addToWhitelist(investor);
+        // vm.stopBroadcast();
 
-        //5.) send manager some gas
-        vm.startBroadcast(vm.envUint("INVESTOR_PRIVATE_KEY")); //0x...A233
-        fundModule.sendStartingGas{value: 0.1 ether}();
+        // //5.) send manager some gas
+        // vm.startBroadcast(vm.envUint("INVESTOR_PRIVATE_KEY")); //0x...A233
+        // fundModule.sendStartingGas{value: 0.1 ether}();
 
-        //6.) approve & invest
-        uint256 amount = 1 * 10 ** 6;
-        usdc.approve(address(fundModule), amount);
-        fundModule.invest(amount);
-        vm.stopBroadcast();
+        // //6.) approve & invest
+        // uint256 amount = 1 * 10 ** 6;
+        // usdc.approve(address(fundModule), amount);
+        // fundModule.invest(amount);
+        // vm.stopBroadcast();
     }
 }
