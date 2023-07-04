c12e1c38d2486d156a6fb6cb6a59baa5390eb843
jonovdm <vdm.jono@gmail.com>
2023-07-04 11:27:21 +0300

    custom decimal erc20 contract for testing

--- src/ERC20Decimal.sol
+++ src/ERC20Decimal.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 //Regular ERC20 contract defaults to 18 decimals
 //so need this contract to set custom decimals for FundModule testing
@@ -11,7 +11,7 @@ contract ERC20Decimal is ERC20 {
 
     constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
         _decimals = decimals_;
-        _mint(msg.sender, 10000*10**decimals_);
+        _mint(msg.sender, 10000 * 10 ** decimals_);
     }
 
     function decimals() public view virtual override returns (uint8) {
