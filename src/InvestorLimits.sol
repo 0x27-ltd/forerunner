f4ef7c6bd78989b31edfd3463c0575c75c2f4322
jonovdm <vdm.jono@gmail.com>
2023-07-06 17:00:48 +0300

    InvestorLimits temp workaround pre compliance file

--- src/InvestorLimits.sol
+++ src/InvestorLimits.sol
@@ -1,27 +1,25 @@
 pragma solidity ^0.8.19;
 
 contract InvestorLimits {
-
     uint256 public _investors;
     uint8 public maxInvestors;
 
-    constructor(
-        uint8 _maxInvestors
-        // address _accountant,
-    ) {
+    constructor(uint8 _maxInvestors) 
+    // address _accountant,
+    {
         maxInvestors = _maxInvestors;
     }
 
-    function addInvestor() internal returns (bool) {
+    //@audit add modifier here so only fund can call this
+    function addInvestor() external returns (bool) {
         //@todo what if the maxInvestors is 0?
         require(_investors < maxInvestors, "Too Many Investors");
         _investors++;
         return true;
     }
 
-    function removeInvestor() internal returns (bool) {
+    function removeInvestor() external returns (bool) {
         _investors--;
         return true;
     }
-
 }
